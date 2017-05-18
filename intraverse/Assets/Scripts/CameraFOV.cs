bc58cf57afd6004a47ca1cd51ae92505c1af3ba2
Ethan Fischer <ethan.f.fischer@gmail.com>
2017-05-17 23:36:23 -0500

    Ethans first commit. Brought in cursor toggle script, added a GameManager, created a new scene called Intraverse

---
+++ intraverse/Assets/Scripts/CameraFOV.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFOV : MonoBehaviour {
+
+    public float fov;
+    public float focusRate;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+    if (Input.GetKeyDown("e"))
+    {
+    //    float updateRate = 3f;
+    //    float tick = 0f;
+    //    while(fov < 150)
+    //    {
+    //        if(tick % 120 > 0.5f)
+    //        {
+    //             fov = fov + focusRate;   
+    //        }
+    //        else
+    //        {
+    //            tick += .0001f;
+    //        }
+            if(fov > 60f)
+            {
+                fov -= 10f;
+            }
+            else
+            {
+                fov = 60f;
+            }
+        // }
+
+
+    }
+    else if(Input.GetKeyDown("q"))
+    {
+        if (fov < 150f)
+        {
+           fov += 10f;
+        }
+        else
+        {
+            fov = 150f;
+        }
+
+    }
+
+    
+    	   GetComponent<Camera>().fieldOfView = fov;
+	}
+}

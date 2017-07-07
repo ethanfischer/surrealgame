5d08b119a18646a264247c862101cdc7b710b9f1
misterfischer <ethan.f.fischer@gmail.com>
2017-07-06 20:00:28 -0500

    getting ready for gear vr

---
+++ intraverse/Assets/Scripts/ClickDetector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace S3
+{
+	public class ClickDetector : MonoBehaviour
+	{
+
+		public int button = 0;
+		public float clickSize = 50; // this might be too small
+
+		void ClickHappened()
+		{
+			Debug.Log("CLICK!");
+		}
+
+		Vector3 pos;
+		void Update()
+		{
+			if (Input.GetMouseButtonDown(button))
+				pos = Input.mousePosition;
+
+			if (Input.GetMouseButtonUp(button))
+			{
+				var delta = Input.mousePosition - pos;
+				if (delta.sqrMagnitude < clickSize * clickSize)
+					ClickHappened();
+			}
+		}
+	}
+}
+	

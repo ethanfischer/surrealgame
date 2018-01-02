bd0d904c7f306a37cbaa380b5a517298785ecbc9
EthanFischer <ethan.f.fischer@gmail.com>
2018-01-01 22:50:15 -0600

    Added wavydistortion to camera. Shaders are nifty
    https://www.youtube.com/watch?v=C0uJ4sZelio
    
    Listen to this nerd describe how to implement them.
    
    I guess shaders do a lot more than I realized.

---
+++ surreal_game/Assets/Scripts/ShaderScripts/WavyDistortionScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class WavyDistortionScript : MonoBehaviour
+{
+
+    public Material mat;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, mat);
+    }
+}

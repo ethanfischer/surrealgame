350dedf5f47670927bb452263aecedfbe8da3758
EthanFischer <ethan.f.fischer@gmail.com>
2018-01-09 22:52:07 -0600

    Created and added new models. Kitchen is coming along

--- surreal_game/Assets/3rd-Party Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ surreal_game/Assets/3rd-Party Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -27,6 +27,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         [SerializeField] private AudioClip[] m_FootstepSounds;    // an array of footstep sounds that will be randomly selected from.
         [SerializeField] private AudioClip m_JumpSound;           // the sound played when character leaves the ground.
         [SerializeField] private AudioClip m_LandSound;           // the sound played when character touches back on ground.
+        public bool muteFootsteps; 
 
         private Camera m_Camera;
         private bool m_Jump;
@@ -162,6 +163,8 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
         private void PlayFootStepAudio()
         {
+            if (muteFootsteps) return;
+
             if (!m_CharacterController.isGrounded)
             {
                 return;

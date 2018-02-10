7b29d10ecd239d8088d1c23362739e9c974bad97
EthanFischer <ethan.f.fischer@gmail.com>
2018-02-10 00:42:42 -0600

    Disappearing kitchen on the move

---
+++ surreal_game/Assets/DontDestroyOnLoad.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SurrealGame
+{
+    public class  DontDestroyOnLoad : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(transform.gameObject);
+        }
+
+        void Start()
+        {
+            SetInitialReferences();
+			
+        }
+
+        void Update()
+        {
+			
+        }
+
+        private void SetInitialReferences()
+        {
+        }
+    }
+}

de8e3727d347289d9b08f0ee6f93231a649a66e5
EthanFischer <ethan.f.fischer@gmail.com>
2018-02-24 17:58:31 -0600

    Additive scene is working. Main scene loads kitchen
    Persistence/static objects and logic is all in Main Scene

---
+++ surreal_game/Assets/Initialize_Scenes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SurrealGame
+{
+    public class Initialize_Scenes : MonoBehaviour
+    {
+        public string initialLocation = "Kitchen";
+
+        void Awake()
+        {
+            LoadSceneAdditively(initialLocation);
+        }
+
+        private static void LoadSceneAdditively(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        }
+    }
+}

d09fcb936e1fc7fc58bc9bb6d579094ba44c685c
EthanFischer <ethan.f.fischer@gmail.com>
2018-02-24 18:48:34 -0600

    ctrl 4 loads main scenes and plays

---
+++ surreal_game/Assets/Editor/LoadMainSceneOnPlay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class LoadMainSceneOnPlay : EditorWindow
+{
+    [MenuItem("Edit/Open Main Scene on Play %4")]
+    static void Main()
+    {
+        if (EditorApplication.isPlaying == true)
+        {
+            EditorApplication.isPlaying = false;
+            return;
+        }
+
+        EditorSceneManager.OpenScene("Assets/Scenes/Main.unity");
+        EditorApplication.isPlaying = true;
+    }
+}

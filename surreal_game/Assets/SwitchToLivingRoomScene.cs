dc1f77645be1d0579d70fc6d1c4525dbf3f1e003
EthanFischer <ethan.f.fischer@gmail.com>
2018-02-28 23:21:32 -0600

    DavidGreeting to LivingRoomChat working

---
+++ surreal_game/Assets/SwitchToLivingRoomScene.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SurrealGame
+{
+    public class SwitchToLivingRoomScene : SwitchSceneOnClick
+    {
+        protected override bool CanSwitchScene()
+        {
+            return true;
+        }
+    }
+}

1b6d3acb9b4ab51e01ffd8e721b58c0720596196
EthanFischer <ethan.f.fischer@gmail.com>
2018-05-16 21:47:39 -0500

    Adds initial VR support

--- surreal_game/Assets/Scripts/Examine.cs
+++ surreal_game/Assets/Scripts/Examine.cs
@@ -40,6 +40,7 @@ namespace SurrealGame
             if(collider == null)
             {
                 gameObject.AddComponent<BoxCollider>();
+                gameObject.GetComponent<BoxCollider>().size = new Vector3(.3f, .3f, .3f);
             }
         }
 

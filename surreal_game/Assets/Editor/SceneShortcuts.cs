805005451a1b07e0e6a42f819aea0c75ffaa6e03
EthanFischer <ethan.f.fischer@gmail.com>
2018-08-05 15:43:26 -0500

    Adjust counter height, Add scene shortcuts

---
+++ surreal_game/Assets/Editor/SceneShortcuts.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+class SceneShortcuts : EditorWindow
+{
+
+    [MenuItem("Scenes/Main _%4")]
+    public static void Main()
+    {
+        EditorSceneManager.OpenScene("Assets/Scenes/Main.unity");
+    }
+
+    [MenuItem("Scenes/House_Kitchen _%5")]
+    public static void House_Kitchen()
+    {
+        EditorSceneManager.OpenScene("Assets/Scenes/House/House_Kitchen.unity");
+    }
+}

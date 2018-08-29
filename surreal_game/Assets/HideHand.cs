184cce857596622bfa6a3643e4566c1ac10057a3
EthanFischer <ethan.f.fischer@gmail.com>
2018-08-28 19:50:59 -0500

    Hide hands with events instead of update()

--- surreal_game/Assets/HideHand.cs
+++ surreal_game/Assets/HideHand.cs
@@ -3,34 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using VRTK;
 
-public class HideHand : MonoBehaviour {
+public class HideHand : MonoBehaviour
+{
     public VRTK_AvatarHandController handController;
     public SkinnedMeshRenderer VRTK_BasicHand;
 
-	// Use this for initialization
-	void Start () {
-        if(handController == null)
+    // Use this for initialization
+    void Start()
+    {
+        if (handController == null)
         {
             Debug.LogError("Missing handController in HideHands script");
         }
-        if(VRTK_BasicHand == null)
+        if (VRTK_BasicHand == null)
         {
             Debug.LogError("Missing VRTK_BasicHand in HideHands script");
         }
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if(handController.interactGrab.IsGrabButtonPressed())
-        {
-            Debug.Log("hide hands");
-            VRTK_BasicHand.enabled = false;
-        }
-        else
-        {
-            VRTK_BasicHand.enabled = true;
-        }
-		
-	}
+
+        handController.interactGrab.ControllerStartGrabInteractableObject += Hide;
+        handController.interactGrab.ControllerStartUngrabInteractableObject += Show;
+
+    }
+
+    void Hide(object sender, ObjectInteractEventArgs e)
+    {
+        VRTK_BasicHand.enabled = false;
+    }
+
+    void Show(object sender, ObjectInteractEventArgs e)
+    {
+        VRTK_BasicHand.enabled = true;
+    }
 }

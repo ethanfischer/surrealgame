99208af3ebf4d883ae255db338cd3140114f101c
EthanFischer <ethan.f.fischer@gmail.com>
2018-08-28 21:42:24 -0500

    Adds Reparent script to ControllerGrabable prefab
    Now you can just add prefab as child to gameobjects in scene. Minimal
    setup

---
+++ surreal_game/Assets/ReparentOnStart.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReparentOnStart : MonoBehaviour
+{
+    void Start()
+    {
+        if (transform.childCount == 0)
+        {
+            Rename();
+            Reparent();
+        }
+    }
+
+    private void Rename()
+    {
+        this.name = transform.parent.name;
+        transform.parent.name += "_geometry";
+    }
+
+    private void Reparent()
+    {
+        var initialParent = transform.parent;
+        transform.SetParent(transform.parent.parent);
+        initialParent.SetParent(this.transform);
+    }
+}

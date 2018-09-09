bec0c714193bff648bd824fbdfe8dbb75f17a127
EthanFischer <ethan.f.fischer@gmail.com>
2018-09-09 13:14:23 -0500

    Stash item adds to inventory

--- surreal_game/Assets/Scripts/PlayerScripts/PlayerInventoryVR.cs
+++ surreal_game/Assets/Scripts/PlayerScripts/PlayerInventoryVR.cs
@@ -7,23 +7,32 @@ using UnityEngine;
 
 namespace Assets.Scripts.PlayerScripts
 {
-    public class PlayerInventoryVR : MonoBehaviour, IPlayerInventoryVR
+    public class PlayerInventoryVR : MonoBehaviour, IPlayerInventory
     {
-        public GameObject inventoryGameObject;
+        public GameObject InventoryGameObject;
         private bool wasInitialized = false;
         private GameManager_ToggleInventoryUI InventoryUIScript;
         private Player_Master PlayerMaster;
 
-        List<GameObject> Items;
+        List<GameObject> Items = new List<GameObject>();
 
-        public void Add(List<GameObject> itemsToAdd)
+        public void Add(GameObject itemToAdd)
         {
-            Items.AddRange(itemsToAdd);
+            Items.Add(itemToAdd);
+            AddItemAsChildAndResetTransform(itemToAdd);
         }
 
         public void SelectItem(GameObject itemToSelect)
         {
             throw new NotImplementedException();
+        }
+
+        private void AddItemAsChildAndResetTransform(GameObject gameObject)
+        {
+            gameObject.transform.parent = InventoryGameObject.transform;
+            gameObject.transform.localPosition = Vector3.zero;
+            gameObject.transform.localRotation = Quaternion.identity;
+            gameObject.transform.localScale = Vector3.one;
         }
     }
 }

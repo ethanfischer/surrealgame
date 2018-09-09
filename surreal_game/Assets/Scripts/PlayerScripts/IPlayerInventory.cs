c55db93c6d12aeac57b0c3fc2cc88e2c1aec5bb2
EthanFischer <ethan.f.fischer@gmail.com>
2018-09-09 15:59:40 -0500

    Add IPlayerInventory, setup editor values

---
+++ surreal_game/Assets/Scripts/PlayerScripts/IPlayerInventory.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.PlayerScripts
+{
+    public interface IPlayerInventory
+    {
+        void Add(GameObject itemToAdd);
+        void SelectItem(GameObject itemToSelect);
+    }
+}
---
+++ surreal_game/Assets/Scripts/PlayerScripts/Player_Stash_Item.cs
@@ -0,0 +1,50 @@
+﻿using Assets.Scripts.PlayerScripts;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+namespace Assets.Scripts.PlayerScripts
+{
+
+
+    public class Player_Stash_Item : MonoBehaviour
+    {
+
+        private IPlayerInventory Inventory { get; set; }
+        public VRTK_AvatarHandController Right_VRTK_AvatarHandController;
+        public VRTK_AvatarHandController Left_VRTK_AvatarHandController;
+
+        // Use this for initialization
+        void Start()
+        {
+            SetInitialReferences(); //TODO figure out way to make this more like a constructor
+
+            if (Inventory == null) Debug.LogError("Inventory is null");
+            if (Right_VRTK_AvatarHandController == null) Debug.LogError("Right_VRTK_AvatarHandController  is null");
+            if (Left_VRTK_AvatarHandController == null) Debug.LogError("Left_VRTK_AvatarHandController is null");
+
+            Left_VRTK_AvatarHandController.interactUse.ControllerStartUseInteractableObject += StashItem;
+            Right_VRTK_AvatarHandController.interactUse.ControllerStartUseInteractableObject += StashItem;
+        }
+
+        private void SetInitialReferences()
+        {
+            Inventory = GetComponentInChildren<IPlayerInventory>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+
+        }
+
+        void StashItem(object o, ObjectInteractEventArgs e)
+        {
+            var target = e.target;
+            Inventory.Add(target);
+        }
+    }
+}

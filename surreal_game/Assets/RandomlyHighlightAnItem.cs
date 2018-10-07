769d74b62b6d53162bff6f6f4a0c72531241f132
Ethan Fischer <ethan.f.fischer@gmail.com>
2018-10-06 20:27:22 -0500

    Randomly turn objects upside down

---
+++ surreal_game/Assets/RandomlyHighlightAnItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomlyHighlightAnItem : MonoBehaviour {
+
+	List<Transform> Items = new List<Transform>();
+	GameObject ChosenItem;
+
+	void Start () {
+		SetInitialReferences();
+		InvokeRepeating("TurnUpsideDown", 5, 5);
+	}
+
+	void SetInitialReferences()
+	{
+		foreach(Transform child in transform)
+		{
+			Items.Add(child);
+		}
+	}
+
+    void TurnUpsideDown()
+	{
+		var i = Random.Range(0, Items.Count);
+        ChosenItem = Items[i].gameObject;
+        ChosenItem.transform.localRotation = Quaternion.Euler(180, 0, 0);
+	}
+}
+
+

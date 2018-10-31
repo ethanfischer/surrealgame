3b2d434f3764a8894516c28b6b7c5fe6f9fc6cf8
EthanFischer <ethan.f.fischer@gmail.com>
2018-10-30 21:53:27 -0500

    Revert "Rename Main to Application.Launch"
    This reverts commit 93c01ba6d8e1cf2dd7c17b44e37a958d9da6bf2e.

--- surreal_game/Assets/Editor/SceneShortcuts.cs
+++ surreal_game/Assets/Editor/SceneShortcuts.cs
@@ -1,32 +1,24 @@
 ﻿using UnityEditor;
 using UnityEditor.SceneManagement;
-using UnityEngine;
 
 class SceneShortcuts : EditorWindow
 {
 
-    [MenuItem("Scenes/Application.Launch _%;4")]
+    [MenuItem("Scenes/Main _%4")]
     public static void Main()
     {
-        SwitchToScene("Assets/Scenes/Application.Launch.unity");
+        EditorSceneManager.OpenScene("Assets/Scenes/Main.unity");
     }
 
-    [MenuItem("Scenes/House_Kitchen _%;5")]
+    [MenuItem("Scenes/House_Kitchen _%5")]
     public static void House_Kitchen()
     {
-        SwitchToScene("Assets/Scenes/House/House_Kitchen.unity");
+        EditorSceneManager.OpenScene("Assets/Scenes/House/House_Kitchen.unity");
     }
 
-    [MenuItem("Scenes/Base_House _%;6")]
+    [MenuItem("Scenes/Base_House _%6")]
     public static void Base_House()
     {
-        SwitchToScene("Assets/Scenes/House/Base_House.unity");
-    }
-
-
-    private static void SwitchToScene(string scene)
-    {
-        EditorSceneManager.OpenScene(scene);
-        Debug.Log("Switched to scene: " + scene);
+        EditorSceneManager.OpenScene("Assets/Scenes/House/Base_House.unity");
     }
 }

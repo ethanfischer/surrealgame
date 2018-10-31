a32c5303c72d2877f5b507c58d8d05bd4295890a
EthanFischer <ethan.f.fischer@gmail.com>
2018-10-30 21:13:36 -0500

    BLOBS and scene changes (also some script changes?)

---
+++ surreal_game/Assets/Scripts/RandomlyAffectItem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomlyAffectItem : MonoBehaviour {
+
+	List<Transform> Items = new List<Transform>();
+	GameObject ChosenItem;
+
+	void Start () {
+		SetInitialReferences();
+		StartCoroutine(AffectItem());
+	}
+
+	void SetInitialReferences()
+    {
+        InitializeItems();
+    }
+
+    IEnumerator AffectItem()
+    {
+        Debug.Log("1");
+        yield return new WaitForSeconds(HeartBeat.Pace);
+
+        Debug.Log("2");
+        TurnItemUpsidedDown();
+    }
+
+    void TurnItemUpsidedDown()
+    {
+        var i = Random.Range(0, Items.Count);
+        ChosenItem = Items[i].gameObject;
+        ChosenItem.transform.localRotation = Quaternion.Euler(180, 0, 0);
+        Debug.Log("AffectItem");
+    }
+
+    void InitializeItems()
+    {
+        foreach (Transform child in transform)
+        {
+            Items.Add(child);
+        }
+    }
+}

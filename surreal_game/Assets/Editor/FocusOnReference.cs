92ec738aaa488a5facade7379ab04c0dbf81ae51
EthanFischer <ethan.f.fischer@gmail.com>
2018-11-05 20:35:45 -0600

    Focus on reference

---
+++ surreal_game/Assets/Editor/FocusOnReference.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ReferenceFocuser : MonoBehaviour
+{
+    [MenuItem("GameObject/FocusOnReference _%0")]
+    public static void FocusOnReference()
+    {
+        Selection.activeGameObject = GameObject.FindGameObjectWithTag("FocusReference");
+        SceneView.FrameLastActiveSceneView();
+    }
+}

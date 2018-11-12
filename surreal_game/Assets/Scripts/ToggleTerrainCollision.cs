0fe86e560643a976035127db2a466c5e9c975850
EthanFischer <ethan.f.fischer@gmail.com>
2018-11-11 19:45:17 -0600

    Render queue + inivisible mask working WIP: toggling terrain-player collision

---
+++ surreal_game/Assets/Scripts/ToggleTerrainCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SurrealGame;
+using UnityEngine;
+
+public class ToggleTerrainCollision : MonoBehaviour
+{
+    public TerrainCollider TerrainCollider;
+    public Collider PlayerCollider;
+
+    void Start()
+    {
+        SetInitialReferences();
+    }
+
+    private void SetInitialReferences()
+    {
+        //if(PlayerCollider == null) Debug.LogError("player collider is null");
+        //if(TerrainCollider == null) Debug.LogError("terrain collider is null");
+    }
+
+    void OnTriggerEnter()
+    {
+        Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Terrain"), true);
+    }
+
+    void OnTriggerExit()
+    {
+        Physics.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Terrain"), false);
+    }
+}

dea2ec0f986ac2a9731b4339a7de49766bb9cab6
EthanFischer <ethan.f.fischer@gmail.com>
2019-01-19 19:06:14 -0600

    Extensions. Comlain if null

---
+++ surreal_game/Assets/Scripts/ToggleObjectVisibility.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts.Extensions;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ToggleObjectVisibility : MonoBehaviour
+    {
+        public GameObject Lens;
+
+        void Start()
+        {
+            SetInitialReferences();
+        }
+
+        private void SetInitialReferences()
+        {
+            Lens.ComplainIfNull();
+        }
+
+        void OnTriggerEnter()
+        {
+            Lens.SetActive(false);
+        }
+
+        void OnTriggerExit()
+        {
+            Lens.SetActive(true);
+        }
+    }
+}

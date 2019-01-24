3123ea42b01378605e8833c8ad8c8f465b401f00
EthanFischer <ethan.f.fischer@gmail.com>
2019-01-23 21:56:27 -0600

    Give fruit gravity on grab

---
+++ surreal_game/Assets/GiveGravityOnGrab.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Scripts.ExtensionsAndUtilities;
+using UnityEngine;
+using VRTK.UnityEventHelper;
+
+namespace Assets
+{
+    public class GiveGravityOnGrab : MonoBehaviour {
+        private Rigidbody RigidBody { get; set; }
+
+        // Use this for initialization
+        void Start ()
+        {
+            RigidBody = GetComponent<Rigidbody>();
+            RigidBody.gameObject.ComplainIfNull();
+        }
+
+        public void GiveGravity()
+        {
+            RigidBody.useGravity = true;
+        }
+
+        // Update is called once per frame
+        void Update () {
+		
+        }
+    }
+}

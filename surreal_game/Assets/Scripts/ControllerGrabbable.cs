25ce8f54e5c58b7d5ec57a4610e869994bd39193
EthanFischer <ethan.f.fischer@gmail.com>
2019-01-29 20:44:57 -0600

    THrowing flowers appears to be destroying them properly

---
+++ surreal_game/Assets/Scripts/ControllerGrabbable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ControllerGrabbable
+    {
+        private object _controllerGrabbable;
+        public void CreateControllerGrabbablePrefab()
+        {
+            _controllerGrabbable = Resources.Load("ControllerGrabable");
+
+
+        }
+
+    }
+}

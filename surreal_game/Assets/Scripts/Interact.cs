df2257cc2874a7bffe021c386586a77fc1b78e59
EthanFischer <ethan.f.fischer@gmail.com>
2019-02-02 15:36:13 -0600

    Add OnPlayerInteraction event that gets called with you touch derrick

--- surreal_game/Assets/Scripts/Interact.cs
+++ surreal_game/Assets/Scripts/Interact.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 namespace Assets.Scripts
 {
-    public class Interact : MonoBehaviour {
+    public class Interact : MonoBehaviour
+    {
+        public event PlayerInteractionEvent PlayerInteractionEvent;
 
         private void OnTriggerEnter(Collider collider)
         {
             if (collider.IsPlayer())
             {
+                OnPlayerInteraction(new OnPlayerInteractionArgs());
                 SystemSounds.Asterisk.Play();
                 Debug.Log("!!!!!");
             }
         }
+
+        protected virtual void OnPlayerInteraction(OnPlayerInteractionArgs args)
+        {
+            PlayerInteractionEvent?.Invoke(this, args);
+        }
     }
+
+    public delegate void PlayerInteractionEvent(object sender, OnPlayerInteractionArgs args);
 }

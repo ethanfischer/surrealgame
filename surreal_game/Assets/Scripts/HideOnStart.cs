72798633c9e19c0f74e48b8a5521e1fbe4a4f790
EthanFischer <ethan.f.fischer@gmail.com>
2019-02-02 14:49:01 -0600

    Hide derrick on start

---
+++ surreal_game/Assets/Scripts/HideOnStart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SurrealGame
+{
+    public class HideOnStart : MonoBehaviour
+    {
+        Player_Dialogue playerDialogue;
+
+        void Start()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}

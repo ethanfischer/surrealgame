ce141a8c56046edc15a657a80e8ba1fcf61c1fdb
EthanFischer <ethan.f.fischer@gmail.com>
2019-02-02 14:14:04 -0600

    Trigger Derrick appearance

--- surreal_game/Assets/Scripts/TriggerDerrickAppearance.cs
+++ surreal_game/Assets/Scripts/TriggerDerrickAppearance.cs
@@ -6,13 +6,14 @@ namespace Assets.Scripts
 {
     public class TriggerDerrickAppearance : MonoBehaviour
     {
+        public GameObject Derrick;
+
         void OnTriggerEnter(Collider collider)
         {
             var go = collider.gameObject;
             if (go.tag != Tags.DISPOSABLE) return;
 
-            Debug.Log("derick!!!!!");
-            SystemSounds.Exclamation.Play();
+            Derrick.SetActive(true);
         }
     }
 }

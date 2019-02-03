a90b2690fa90115b72bb5cd5fdbf2fc5160ab40f
EthanFischer <ethan.f.fischer@gmail.com>
2019-02-02 19:01:46 -0600

    Play pop when grabbing fruit

---
+++ surreal_game/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlaySound : MonoBehaviour
+    {
+        public void Play()
+        {
+            gameObject.GetComponentInChildren<AudioSource>().Play();
+        }
+    }
+}

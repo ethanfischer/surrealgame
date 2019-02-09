fb4f05d7c337acec1090004eed62569ae5d6890e
EthanFischer <ethan.f.fischer@gmail.com>
2019-02-08 22:01:19 -0600

    Advance dialogue by touching his face

--- surreal_game/Assets/Scripts/Interact.cs
+++ surreal_game/Assets/Scripts/Interact.cs
@@ -1,21 +1,45 @@
-﻿using System.Media;
+﻿using System;
+using System.Media;
 using Assets.Scripts.ExtensionsAndUtilities;
 using Assets.Scripts.PlayerScripts;
 using SurrealGame;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts
 {
     public class Interact : MonoBehaviour
     {
         public GameObject DialogueUI;
+        public Text Text;
 
-        private void OnTriggerEnter(Collider collider)
+        private int _dialogueCounter = 0;
+
+        public void AdvanceDialogue()
         {
-            if (collider.IsPlayer())
+            DialogueUI.SetActive(true);
+
+            switch (_dialogueCounter)
             {
-                DialogueUI.SetActive(true);
+                case 0:
+                    Text.text = "You dropped something.";
+                    break;
+                case 1:
+                    Text.text = "Enjoy";
+                    break;
+                case 2:
+                    GiveKey();
+                    break;
+                default:
+                    break;
             }
+
+            _dialogueCounter++;
+        }
+
+        private void GiveKey()
+        {
+            Debug.Log("!!!!!");
         }
     }
 }

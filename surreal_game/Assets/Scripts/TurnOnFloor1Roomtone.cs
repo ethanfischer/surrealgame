ae6b0fe343260d0da9bbdaa54570d3bf9057a310
EthanFischer <ethan.f.fischer@gmail.com>
2019-02-15 20:16:15 -0600

    Adds floor1 roomatone trigger

---
+++ surreal_game/Assets/Scripts/TurnOnFloor1Roomtone.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts.ExtensionsAndUtilities;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TurnOnFloor1Roomtone : MonoBehaviour
+    {
+        public AudioSource Floor1Roomtone;
+
+        void OnTriggerEnter(Collider collider)
+        {
+            if (collider.IsPlayer())
+            {
+                Floor1Roomtone.Play();
+            }
+        }
+    }
+}

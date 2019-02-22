647874293d3ce0c238657c292663dd66419161bd
EthanFischer <ethan.f.fischer@gmail.com>
2019-02-21 21:05:28 -0600

    Add Ethan height for scale

---
+++ surreal_game/Assets/Scripts/XRSettings.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+namespace Assets.Scripts
+{
+    public class XRSettingsManager : MonoBehaviour
+    {
+        public void DOSomething()
+        {
+        XRSettings.
+        }
+
+
+    }
+}

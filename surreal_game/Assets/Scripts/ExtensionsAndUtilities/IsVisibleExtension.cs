e0158ac508d4c9983adfc14ae9b679c8f8820f7c
EthanFischer <ethan.f.fischer@gmail.com>
2019-03-15 21:22:58 -0500

    Add protocomb music and WIP visibility events

---
+++ surreal_game/Assets/Scripts/ExtensionsAndUtilities/IsVisibleExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.ExtensionsAndUtilities
+{
+    public static class RendererExtensions
+    {
+        public static bool IsVisibleFrom(this Renderer renderer, Camera camera)
+        {
+            Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
+            return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+        }
+    }
+}
---
+++ surreal_game/Assets/Scripts/IsVisbleEvents.cs
@@ -0,0 +1,42 @@
+﻿using Assets.Scripts.ExtensionsAndUtilities;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class IsVisbleEvents : MonoBehaviour
+    {
+        public event VisibilityChangedEvent VisibilityChangedEvent;
+        public bool IsVisible { get; set; }
+        private Renderer _renderer;
+
+        // Use this for initialization
+        void Start()
+        {
+            _renderer = GetComponent<Renderer>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var isVisible = _renderer.IsVisibleFrom(Camera.main);
+            if (isVisible != IsVisible)
+            {
+                VisibilityChangedEvent.Invoke(this, new VisibilityChangedEventArgs(isVisible));
+                IsVisible = isVisible;
+                Debug.Log("Visibility changed to" + IsVisible);
+            }
+        }
+    }
+
+    public delegate void VisibilityChangedEvent(object sender, VisibilityChangedEventArgs args);
+
+    public class VisibilityChangedEventArgs
+    {
+        public bool IsVisible;
+
+        public VisibilityChangedEventArgs(bool isVisible)
+        {
+            IsVisible = isVisible;
+        }
+    }
+}

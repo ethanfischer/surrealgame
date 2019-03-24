a9e178055effad746e7e92ed7f02e2dc76d2c8d6
EthanFischer <ethan.f.fischer@gmail.com>
2019-03-24 18:33:58 -0500

    Add PauseUnpause script to fix bodycollidercontainer issue

---
+++ surreal_game/Assets/Scripts/PauseUnpause.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PauseUnpause : MonoBehaviour
+    {
+        void Start()
+        {
+            Time.timeScale = 0;
+            Time.timeScale = 1;
+        }
+    }
+}

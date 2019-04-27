96962e57edd3723bd7262756facbe3320ffb5429
EthanFischer <ethan.f.fischer@gmail.com>
2019-04-27 18:36:57 -0500

    WIP repeatX

---
+++ surreal_game/Assets/Scripts/RepeatX.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class RepeatX : MonoBehaviour
+    {
+        public int RepeatCount = 0;
+        public float GapSize = 1f;
+        void Start()
+        {
+            for (var i = 0; i < RepeatCount; i++)
+            {
+                var clone = GameObject.Instantiate(this.gameObject);
+                var pos = clone.transform.localPosition;
+                clone.transform.localPosition = new Vector3(pos.x + (GapSize * i), pos.y, pos.z);
+            }
+        }
+    }
+}

1f9fd25be0d4709fe0e5bc38ef6d51190fda0984
EthanFischer <ethan.f.fischer@gmail.com>
2019-04-29 22:42:16 -0500

    WIP

---
+++ surreal_game/Assets/Scripts/GrabAction.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GrabAction : MonoBehaviour
+    {
+        public SoundEffectsManager.AudioClipType SoundEffect;
+
+        public void PlaySoundEffect()
+        {
+            SoundEffectsManager.Play(SoundEffect);
+        }
+    }
+}
---
+++ surreal_game/Assets/Scripts/SoundEffectsManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class SoundEffectsManager : MonoBehaviour
+    {
+        private static SoundEffectsManager _instance = null;
+        public static SoundEffectsManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = GameObject.FindObjectOfType<SoundEffectsManager>();
+                }
+
+                return _instance;
+            }
+        }
+        public AudioClip[] AudioClips;
+        public AudioSource AudioSource;
+
+        public enum AudioClipType
+        {
+            Unlock = 0,
+            ItemPickup = 1,
+            Lock = 2,
+            Doorbell = 3
+        }
+
+        public static void Play(AudioClipType sfxType)
+        {
+            Instance.PlaySfx(sfxType);
+        }
+
+        private void PlaySfx(AudioClipType sfxType)
+        {
+            Debug.Log(sfxType);
+            AudioSource.clip = AudioClips[Convert.ToInt32(sfxType)];
+            Debug.Log(AudioSource);
+            AudioSource.Play();
+        }
+    }
+}

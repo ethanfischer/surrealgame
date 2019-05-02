10692b2b7867f7832330a889793554b88ac35493
EthanFischer <ethan.f.fischer@gmail.com>
2019-05-01 19:46:49 -0500

    One shot grab sound

--- surreal_game/Assets/Scripts/GrabAction.cs
+++ surreal_game/Assets/Scripts/GrabAction.cs
@@ -5,10 +5,14 @@ namespace Assets.Scripts
     public class GrabAction : MonoBehaviour
     {
         public SoundEffectsManager.AudioClipType SoundEffect;
+        public bool IsOneShot = false;
+        private int _grabCount = 0;
 
         public void PlaySoundEffect()
         {
+            if (IsOneShot && _grabCount > 0) return;
             SoundEffectsManager.Play(SoundEffect);
+            _grabCount++;
         }
     }
 }

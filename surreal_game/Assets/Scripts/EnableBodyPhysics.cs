1f57f12d48f0cd30f29b02a270a39f2ed31af29c
EthanFischer <ethan.f.fischer@gmail.com>
2019-05-21 21:53:14 -0500

    Enable body physics on end dialogue

---
+++ surreal_game/Assets/Scripts/EnableBodyPhysics.cs
@@ -0,0 +1,13 @@
+﻿using Assets.Scripts.PlayerScripts;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class EnableBodyPhysics : MonoBehaviour
+    {
+        public void Do()
+        {
+            Player.Instance.BodyPhysics.enabled = true;
+        }
+    }
+}
--- surreal_game/Assets/Scripts/PlayerScripts/Player.cs
+++ surreal_game/Assets/Scripts/PlayerScripts/Player.cs
@@ -9,6 +9,7 @@ namespace Assets.Scripts.PlayerScripts
         public PlayerInventoryVR Inventory;
         public GameObject Avatar;
         public VRTK_SDKManager VRTK_SDKManager;
+        public VRTK_BodyPhysics BodyPhysics;
 
         private static Player _instance = null;
         public static Player Instance
@@ -32,6 +33,16 @@ namespace Assets.Scripts.PlayerScripts
         private void Start()
         {
             Avatar.ComplainIfNull();
+        }
+
+        public void EnableBodyPhysics()
+        {
+            VRTK_SDKManager.GetComponent<VRTK_BodyPhysics>().enabled = true;
+        }
+
+        public void DisableBodyPhysics()
+        {
+            VRTK_SDKManager.GetComponent<VRTK_BodyPhysics>().enabled = false;
         }
     }
 }
